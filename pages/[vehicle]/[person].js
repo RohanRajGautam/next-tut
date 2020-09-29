bf204ead24ae71915885b8b170a4103c54d00b33
rohan <rohanrajgautam@gmail.com>
2020-09-29 10:34:24 +0545

    added mock server, getinitialprops example

--- pages/[vehicle]/[person].js
+++ pages/[vehicle]/[person].js
@@ -1,8 +1,37 @@
 import { useRouter } from 'next/router';
-
-export default function Person() {
+import { useState, useEffect } from 'react';
+export default function Person({ ownerList }) {
   const router = useRouter();
-  console.log(router.query);
+  const [owners, setOwners] = useState(ownerList);
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch(
+        `http://localhost:4001/vehicles?ownerName='${query.person}'&vehicle='${query.vehicle}'`
+      );
+      const ownerList = await response.json();
+      setOwners(ownerList);
+    }
 
-  return <h2>Rohan's car</h2>;
+    if (ownerList[0] == 0) {
+      loadData();
+    }
+  }, []);
+
+  if (!owners[0]) {
+    <div>loading...</div>;
+  }
+
+  return <pre>{owners[0]?.details}</pre>;
 }
+
+Person.getInitialProps = async (ctx) => {
+  if (!ctx.req) {
+    return { ownerList: [] };
+  }
+  const { query } = ctx;
+  const response = await fetch(
+    `http://localhost:4001/vehicles?ownerName='${query.person}'&vehicle='${query.vehicle}'`
+  );
+  const ownerList = await response.json();
+  return { ownerList: ownerList };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,3 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
 export default function Home() {
-  return (
-    <div>
-      <h2>yo</h2>
-    </div>
-  );
+  return <h1>Home</h1>;
 }
